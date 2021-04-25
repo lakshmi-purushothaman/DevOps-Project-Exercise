90304df3d285790ae50dacdcfa5ea1aa7d2a0b37
Lakshmi <lakshmi.purushothaman@aexp.com>
2021-04-25 09:54:43 +0100

    Made changes as per PR comments by Hugh

--- todo_app/app.py
+++ todo_app/app.py
@@ -10,18 +10,16 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    #return 'Hello World!'
 #Code that fetches all the TODO items from session
     items = get_items()
     return render_template('index.html', items=items)
 
-@app.route('/addItem', methods=['POST','GET'])
-def formHandler():
+@app.route('/addItem', methods=['POST'])
+def addItem():
 #Code that adds the TODO item added on the screen to a session
-    if request.method == 'POST':
-        title = request.form.get("title")
-        item = add_item(title)
-        return redirect(f"/")
+    title = request.form.get("title")
+    item = add_item(title)
+    return redirect(f"/")
 
 if __name__ == '__main__':
     app.run()
